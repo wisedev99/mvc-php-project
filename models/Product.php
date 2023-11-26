979a97ee18f0c1dec049665a1fa1deb3e1b6991d
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2023-11-26 13:34:50 +0500

    method for get product url

--- models/Product.php
+++ models/Product.php
@@ -11,9 +11,50 @@ class Product
 
     public function all()
     {
-        $query = "SELECT * FROM products";
+        $query = "SELECT * FROM products WHERE type = 'item'";
         $stmt = $this->conn->query($query);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        return array_map(function ($product) {
+            if ($product['type'] == 'item') {
+                $product['media_url'] = "/store/" . $product['model_number'] . ".png"; // Replace 'new_key' and 'new_value' with your desired key-value pair
+            }
+            return $product;
+        }, $stmt->fetchAll(PDO::FETCH_ASSOC));
     }
 
+    public function getTaskById($taskId)
+    {
+        $query = "SELECT * FROM tasks WHERE id = :taskId";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':taskId', $taskId);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function createTask($title, $content)
+    {
+        $query = "INSERT INTO tasks (title, content) VALUES (:title, :content)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->execute();
+    }
+
+    public function updateTask($taskId, $title, $content)
+    {
+        $query = "UPDATE tasks SET title = :title, content = :content WHERE id = :taskId";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':taskId', $taskId);
+        $stmt->execute();
+    }
+
+    public function deleteTask($taskId)
+    {
+        $query = "DELETE FROM tasks WHERE id = :taskId";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':taskId', $taskId);
+        $stmt->execute();
+    }
 }
