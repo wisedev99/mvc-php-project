fca2f3254b4d0c49e3604e0d5de3be8fa991cd98
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2023-11-27 03:15:49 +0500

    fixed error in get data by params

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 // Web-based routes
+$url = $_SERVER['REQUEST_URI'];
+
+// Remove any query parameters from the URL
+$urlParts = explode('?', $url);
+$url = $urlParts[0];
+
 $routes = [
     '/' => 'ProductController@index',
+    '/products' => 'ProductController@allProducts',
+    '/category' => 'ProductController@getProductByCat',
 ];
 
-// Loop through the routes and determine which controller and action to invoke
-foreach ($routes as $url => $controllerAction) {
+foreach ($routes as $route => $controllerAction) {
     [$controllerName, $actionName] = explode('@', $controllerAction);
 
-    if ($_SERVER['REQUEST_URI'] === $url) {
+    if ($url === $route) {
         $controllerClassName = $controllerName;
 
         $controller = new $controllerClassName();
-        $controller->$actionName();
+        $routeParams = [];
+        if ($route === '/category') {
+            $categoryName = trim($url, '/');
+            $routeParams[] = $categoryName;
+        }
+
+        $controller->$actionName(...$routeParams);
+
         break;
     }
 }
