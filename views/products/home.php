0d2b0edd41b10f338309b6ff9e98ce0b595ffc93
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2023-11-29 21:25:46 +0500

    changed images url for showing in server

--- views/products/home.php
+++ views/products/home.php
@@ -1,5 +1,5 @@
 <div class="w-full">
-    <img src="/assets/images/banner.jpg" class="w-full  md:h-64 " alt="">
+    <img src="/public/public/public/assets/images/banner.jpg" class="w-full  md:h-64 " alt="">
     <div class="mx-4 md:mx-2">
         <div class="mx-auto max-w-2xl my-10 text-center">
             <div class=" space-y-4">
@@ -13,15 +13,15 @@
             <h1 class="font-bold">Featured selections</h1>
             <div class="grid grid-cols-2 gap-4 justify-center">
                 <div class="">
-                    <img src="/assets/images/featured_selections/mens-watches_sky_dweller_portrait.jpg" class="w-full h-72" alt="">
+                    <img src="/public/public/public/assets/images/featured_selections/mens-watches_sky_dweller_portrait.jpg" class="w-full h-72" alt="">
                     <h3 class="font-bold text-sm uppercase">Rolex Men's watches</h3>
                 </div>
                 <div class="">
-                    <img src="/assets/images/featured_selections/womens-watches_pearlmaster_39_portrait.jpg" class="w-full h-72" alt="">
+                    <img src="/public/public/assets/images/featured_selections/womens-watches_pearlmaster_39_portrait.jpg" class="w-full h-72" alt="">
                     <h3 class="font-bold text-sm uppercase">Rolex women's watches</h3>
                 </div>
                 <div class="col-span-2 ">
-                    <img src="/assets/images/featured_selections/gold-watches_portrait.jpg" class="w-full h-72" alt="">
+                    <img src="/public/public/assets/images/featured_selections/gold-watches_portrait.jpg" class="w-full h-72" alt="">
                     <h3 class="font-bold text-sm uppercase">Rolex Gold watches</h3>
 
                 </div>
@@ -30,7 +30,7 @@
 
         <div class="max-w-xl mx-auto my-10">
             <div class="flex flex-col space-y-10 ">
-                <img src="/assets/images/info/experience-a-rolex_portrait.jpg" alt="">
+                <img src="/public/public/assets/images/info/experience-a-rolex_portrait.jpg" alt="">
                 <div class="flex flex-col items-center space-y-4">
                     <h1 class="uppercase text-4xl ">visit us in store</h1>
                     <p>
