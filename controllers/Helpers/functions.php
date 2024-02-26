8335426f28f605982c3581b3c0e3d1bf38b5bf1e
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2024-02-26 12:17:11 +0500

    added request function

--- controllers/Helpers/functions.php
+++ controllers/Helpers/functions.php
@@ -5,3 +5,13 @@ function view($page, $data)
     // $view = $page;
     include '../views/layout.php';
 }
+function test($url, $data)
+{
+
+    include '../views/test.php';
+}
+
+function request($parameter)
+{
+    return $_REQUEST[$parameter];
+}
