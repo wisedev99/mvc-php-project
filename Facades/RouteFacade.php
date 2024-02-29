fe9bb235e9f13ff02176ec9e5f2baf662a8de11f
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2024-02-29 14:52:46 +0500

    last of last comment

--- Facades/RouteFacade.php
+++ Facades/RouteFacade.php
@@ -4,6 +4,7 @@ class RouteFacade
 {
 
     protected static $controller;
+    protected $routes = [];
 
     public static function toAction($class)
     {
@@ -12,4 +13,26 @@ class RouteFacade
         self::$controller = new $name();
         return self::$controller->$action();
     }
+
+    // public function addRoute(string $method, string $url, object $controller, closure $target)
+    // {
+    //     $this->routes[$method][$url] = $controller->$target();
+    // }
+    // public function matchRoute()
+    // {
+    //     $method = $_SERVER['REQUEST_METHOD'];
+    //     $url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    //     var_dump($this->routes);
+    //     if (isset($this->routes[$method])) {
+    //         foreach ($this->routes[$method] as $routeUrl => $target) {
+    //             $pattern = preg_replace('/\/:([^\/]+)/', '/(?P<$1>[^/]+)', $routeUrl);
+    //             if (preg_match('#^' . $pattern . '$#', $url, $matches)) {
+    //                 $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+    //                 call_user_func_array($target, $params);
+    //                 return;
+    //             }
+    //         }
+    //     }
+    //     throw new Exception('Route not found');
+    // }
 }
--- routes/test.php
+++ routes/test.php
@@ -6,3 +6,7 @@ Route::get('/test', 'ProductController@test');
 Route::get('/products', 'ProductController@allProducts');
 Route::get('/category', 'ProductController@getProductByCat');
 Route::get('/all-product-with-children', 'ProductController@getItemsWithChildren');
+
+
+// $route = new Route();
+// $route->matchRoute();
